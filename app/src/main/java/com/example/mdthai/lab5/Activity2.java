f56101407df68f9795055e748bb94f60dd74b6d7
Thai-Vu Nguyen <thaivu22@gmail.com>
2016-09-28 18:00:54 -0400

    Updated Manifest, Activity Layouts and Classes

--- app/src/main/java/com/example/mdthai/lab5/Activity2.java
+++ app/src/main/java/com/example/mdthai/lab5/Activity2.java
@@ -1,17 +1,62 @@
 package com.example.mdthai.lab5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by MDThai on 9/28/2016.
  */
 public class Activity2 extends AppCompatActivity {
 
+    boolean returnBtnHit = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2_main);
+    }
+
+    public void returnMessage(View view)
+    {
+        Button btn = (Button) view;
+        String message;
+        if (btn.getId() == R.id.act2_btn1) {
+            message = getString(R.string.act2_btn1);
+            logMethod("Return Button 1");
+
+        }
+        else {
+            message = getString(R.string.act2_btn2);
+            logMethod("Return Button 2");
+        }
+
+        Intent i = new Intent();
+        i.putExtra("Act2", message);
+        setResult(RESULT_OK, i);
+        returnBtnHit = true;
+        finish();
+
+
+    }
+
+    @Override
+    public void finish(){
+        if(returnBtnHit == false)
+        {
+            Intent i = new Intent();
+            i.putExtra("Act2",getString(R.string.nodata));
+            setResult(RESULT_CANCELED, i);
+        }
+
+        super.finish();
+    }
+
+    public static void logMethod(String message){
+        Log.d("Intent 2", message);
     }
 
 }
--- app/src/main/java/com/example/mdthai/lab5/MainActivity.java
+++ app/src/main/java/com/example/mdthai/lab5/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.mdthai.lab5;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultView = (TextView)findViewById(R.id.act1_result);
+
     }
+
+    public void launchAct2(View view)
+    {
+       Intent i = new Intent(this, Activity2.class);
+
+        logMethod("Launching Activity 2");
+        startActivityForResult(i, 0);
+
+    }
+
+    public void launchAct3(View view)
+    {
+        Intent i = new Intent(this, Activity3.class);
+
+        logMethod("Launching Activity 3");
+        startActivityForResult(i, 0);
+    }
+
+    public void launchBrowser(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.ca/"));
+        startActivity(intent);
+    }
+
+    public void launchGeo(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q="));
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent i)
+    {
+
+        String value = "No return";
+
+        if (result == RESULT_OK){
+            if (i != null)
+            {
+                if(i.hasExtra("Act2"))
+                {
+                    value = i.getExtras().getString("Act2");
+                }
+                else
+                {
+                    value = "No Activity data";
+                }
+
+            }
+        }
+        else
+        {
+            value = "Cancel return";
+        }
+
+        resultView.setText(value);
+
+    }
+
+
+    public static void logMethod(String message){
+        Log.d("Intent 1", message);
+    }
+
+
+
 }
